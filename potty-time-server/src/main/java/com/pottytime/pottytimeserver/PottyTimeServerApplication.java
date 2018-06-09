65d0e6359ada49fb7940b28b76abce85e54f7405
Sean Bucholtz <sean.bucholtz@gmail.com>
2018-06-09 12:57:54 -0700

    Initialized server module

---
+++ potty-time-server/src/main/java/com/pottytime/pottytimeserver/PottyTimeServerApplication.java
@@ -0,0 +1,12 @@
+package com.pottytime.pottytimeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PottyTimeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PottyTimeServerApplication.class, args);
+    }
+}
