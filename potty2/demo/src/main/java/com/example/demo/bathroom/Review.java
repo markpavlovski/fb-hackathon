72505d9e97800b17f32747139f76b66688bfc7bc
Jenny Lian <jennylian100@gmail.com>
2018-06-09 23:35:57 -0700

    added a review entity, each bathroom should have lots of reviews

---
+++ potty2/demo/src/main/java/com/example/demo/bathroom/Review.java
@@ -0,0 +1,49 @@
+package com.example.demo.bathroom;
+
+import javax.persistence.*;
+
+@Entity
+public class Review {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long reviewId;
+
+    @Column(name="content")
+    String Content;
+
+    public long getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(long reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String content) {
+        Content = content;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public Review(String content, String author) {
+
+        Content = content;
+        Author = author;
+    }
+
+    @Column(name="author")
+    String Author;
+
+
+}
